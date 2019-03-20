07ee730db4e182b6ad5e01989cb5e90e31d80369
minyeong98 <dhkgkgk3@naver.com>
2019-03-20 14:51:03 +0900

    Initial Commit

---
+++ app/src/main/java/com/example/user/my/K20190320.java
@@ -0,0 +1,13 @@
+package com.example.user.my;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class K20190320 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_k20190320);
+    }
+}
